4d6a26760990fcf66e74176987cb19a014219bec
Kirill Gorkiy <gorkiy@list.ru>
2022-08-25 17:35:06 +0200

    done

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
@@ -0,0 +1,13 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import java.util.Set;
+
+import ru.kata.spring.boot_security.demo.model.Role;
+
+public interface RoleService {
+    Set<Role> findAll();
+
+    Role findByName(String name);
+
+    void save(Role role);
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,20 @@
+
+package ru.kata.spring.boot_security.demo.service;
+
+import java.util.List;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import ru.kata.spring.boot_security.demo.model.User;
+
+@Service
+public interface UserService extends UserDetailsService {
+    User findById(Long id);
+    List<User> findAll();
+    void saveUser(User user);
+    void deleteById(Long id);
+    User findByUsername(String username);
+    User findByEmail(String email);
+}
+
